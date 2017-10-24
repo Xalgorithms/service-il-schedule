cad3c2bb5ed289b6a857dafa17af5238e0a96b0b
Don Kelly <karfai@gmail.com>
2017-10-24 23:14:37 +0400

    [#3] Add versioning to the build
    Change-Id: Id4cb4a6fae1c38345d5a22cf08f63b8ad2c77bcd

--- build_images.sh
+++ build_images.sh
@@ -1,8 +1,11 @@
-TAG="development"
+ENVIRONMENT="development"
 if [[ -n "$TRAVIS_BRANCH" && "$TRAVIS_BRANCH" == "production" ]]; then
-   TAG=$TRAVIS_BRANCH
+   ENVIRONMENT=$TRAVIS_BRANCH
 fi
 
-docker build -t xalgorithms/xadf-schedule-service:$TAG -f Dockerfile.$TAG .
+VERSION=`cat .version`
+
+docker build -t "xalgorithms/xadf-schedule-service:latest-$ENVIRONMENT" -t "xalgorithms/xadf-schedule-service:$VERSION-$ENVIRONMENT" -f "Dockerfile.$ENVIRONMENT" .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push xalgorithms/xadf-schedule-service:$TAG
+docker push "xalgorithms/xadf-schedule-service:latest-$ENVIRONMENT"
+docker push "xalgorithms/xadf-schedule-service:$VERSION-$ENVIRONMENT"
