6aebd1b4450eed2ad90b5a94ee1d2b04b04233d5
Don Kelly <karfai@gmail.com>
2018-06-12 17:13:04 +0400

    Builds a Scala wrapper around looking for things in BsonDocuments

---
+++ app/models/Document.scala
@@ -0,0 +1,94 @@
+package models
+
+import collection.JavaConverters._
+import collection.immutable
+import org.bson._
+import org.joda.time.DateTime
+
+object Document {
+  def maybe_find_text(doc: BsonDocument, k: String): Option[String] = {
+    maybe_find_value(doc, k) match {
+      case Some(v) => Option(convert_to_string(v))
+      case None    => None
+    }
+  }
+
+  def maybe_find_many_text(doc: BsonDocument, ks: Seq[String]): Seq[String] = {
+    maybe_find_many_values(doc, ks).foldLeft(Seq[String]()) { (seq, v) =>
+      convert_to_string(v) match {
+        case null => seq
+        case (s: String) => seq :+ s
+      }
+    }
+  }
+
+  def maybe_find_document(doc: BsonDocument, k: String): Option[BsonDocument] = {
+    maybe_find_value(doc, k) match {
+      case Some(v) => Option(convert_to_document(v))
+      case None    => None
+    }
+  }
+
+  def maybe_find_date_time(doc: BsonDocument, k: String): Option[DateTime] = {
+     maybe_find_value(doc, k) match {
+      case Some(v) => Option(convert_to_date_time(v))
+      case None    => None
+    }
+  }
+
+  def maybe_find_many_values(doc: BsonDocument, ks: Seq[String]): Seq[BsonValue] = {
+    ks.foldLeft(Seq[BsonValue]()) { (seq, k) =>
+      maybe_find_value(doc, k) match {
+        case Some(v) => seq :+ v
+        case None    => seq
+      }
+    }
+  }
+
+  def maybe_find_value(doc: BsonDocument, k: String): Option[BsonValue] = {
+    maybe_find_value(doc, k.split('.'))
+  }
+
+  def maybe_find_value(doc: BsonDocument, ks: Seq[String]): Option[BsonValue] = ks.size match {
+    case 1 => Option(doc.get(ks.head, null))
+    case len if len > 1 => {
+      maybe_find_value(Option(doc.getDocument(ks.head, null)), ks.tail)
+    }
+    case _ => None
+  }
+
+  def maybe_find_value(opt_doc: Option[BsonDocument], ks: Seq[String]): Option[BsonValue] = opt_doc match {
+    case Some(doc) => maybe_find_value(doc, ks)
+    case None      => None
+  }
+
+  def convert_to_document(v: BsonValue): BsonDocument = v match {
+    case (dv: BsonDocument) => dv
+    case _ => null
+  }
+
+  def convert_to_date_time(v: BsonValue): DateTime = v match {
+    case (nv: BsonString)    => {
+      try {
+        new DateTime(nv.getValue())
+      } catch {
+        case _: Throwable => null
+      }
+    }
+
+    case (nv: BsonTimestamp) => new DateTime(nv.getTime().toLong * 1000)
+    case (nv: BsonDateTime)  => new DateTime(nv.getValue())
+    case _                   => null
+  }
+
+  def convert_to_string(v: BsonValue): String = v match {
+    // case (nv: BsonBoolean)  => nv.toString()
+    // case BsonType.DATE_TIME  => v.asDateTime().()
+    // case BsonType.DOUBLE  => v.asDouble().toString()
+    // case BsonType.INT64  => v.asInt64().toString()
+    // case BsonType.INT32  => v.asInt32().toString()
+    case (nv: BsonString)  => nv.getValue()
+    // case BsonType.TIMESTAMP  => match_value(v.asTimestamp())
+    case _ => null
+  }
+}
---
+++ test/models/DocumentSpec.scala
@@ -0,0 +1,80 @@
+package models
+
+import scala.io.Source
+import org.bson._
+import org.joda.time.DateTime
+import play.api.libs.json._
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest._
+
+import models._
+
+class DocumentSpec extends FlatSpec with Matchers with MockFactory {
+  val doc = BsonDocument.parse(Source.fromFile(s"files/document/0.json").mkString)
+
+  "Document" should "find text elements" in {
+    val expects = Map(
+      "a" -> Some("AA"),
+      "b" -> Some("BB"),
+      "c" -> None,
+      "c.x.p" -> Some("PP")
+    )
+
+    expects.foreach { case (k, ex) =>
+      Document.maybe_find_text(doc, k) shouldEqual(ex)
+    }
+  }
+
+  it should "find many text elements" in {
+    val expects = Seq(
+      Tuple2(Seq("a", "b"), Seq("AA", "BB")),
+      Tuple2(Seq("a", "c"), Seq("AA")),
+      Tuple2(Seq("a", "s", "b", "t", "z"), Seq("AA", "BB")),
+      Tuple2(Seq("c.x.p", "c.x.q"), Seq("PP", "QQ"))
+    )
+
+    expects.foreach { tup =>
+      Document.maybe_find_many_text(doc, tup._1) shouldEqual(tup._2)
+    }
+  }
+
+  it should "find document elements" in {
+    val expects = Map(
+      "a" -> None,
+      "b" -> None,
+      "c" -> Option(doc.getDocument("c", null)),
+      "c.x" -> Option(doc.getDocument("c", null).getDocument("x", null)),
+      "c.z" -> None
+    )
+
+    expects.foreach { case (k, ex) =>
+      Document.maybe_find_document(doc, k) shouldEqual(ex)
+    }
+  }
+
+  it should "find date time elements (as strings)" in {
+    val expects = Map(
+      "a" -> None,
+      "b" -> None,
+      "c" -> None,
+      "d" -> Some(new DateTime("2018-06-12T11:24:47Z")),
+      "e" -> Some(new DateTime("2018-06-12T11:24:58Z"))
+    )
+
+    expects.foreach { case (k, ex) =>
+      Document.maybe_find_date_time(doc, k) shouldEqual(ex)
+    }
+
+    val dt = new DateTime()
+    val ndoc = new BsonDocument()
+    val secs = dt.getMillis / 1000
+
+    ndoc.append("a", new BsonTimestamp(secs.toInt, 0))
+    ndoc.append("b", new BsonDateTime(dt.getMillis))
+
+
+    Document.maybe_find_date_time(ndoc, "a") shouldEqual(Some(new DateTime(secs * 1000)))
+    Document.maybe_find_date_time(ndoc, "b") shouldEqual(Some(dt))
+  }
+}
